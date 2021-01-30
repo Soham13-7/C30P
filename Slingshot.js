b633c1948f5d15551f58c268d03b6fd84d894cc9
Sohamrajsinh Jadeja <sohamrajsinh@gmail.com>
2021-01-30 11:50:56 +0530

    C30

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.2,
             length: 10
         }
         this.sling1 = loadImage('sprites/sling1.png');
@@ -12,7 +12,15 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+
     }
+attach(body)
+{
+    this.sling.bodyA = body;
+}
+
+
+
 
     fly(){
         this.sling.bodyA = null;
